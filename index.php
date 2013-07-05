fcef3b2b9aa8012385a59a4d485afe4a70f393a5
K. Kanchev <no thank you>
2013-07-05 22:50:33 +0300

    edited pagination functionality

--- index.php
+++ index.php
@@ -19,24 +19,28 @@
 							По този начин се променя цвета на заглавието спрямо темплейта, но е много по функционално. -->
 <?php if (have_posts()) : ?>
 	<?php while (have_posts()) : the_post(); ?>
-						<div>
 							<header>
 								<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 							</header>
 		<?php the_content('view more'); ?>
 		<?php //wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
 				<br />
-				</div>
+				<div class="cl"></div>
 				<hr>
 	<?php endwhile; ?>
 				<div class="navigation">
-					<div class="alignleft">
+<?php if ( $wp_query->max_num_pages >1) : ?>
+					<div class="alignleft blue-btn">
 						<?php next_posts_link('&laquo; Older Entries') ?>
 					</div>
-					<div class="alignright">
+					<div class="alignright blue-btn">
 						<?php previous_posts_link('Newer Entries &raquo;') ?>
 					</div>
+<?php else: ?>  
+					<!-- <p>No newer/older posts</p> -->  
+<?php endif; ?>  
 				</div>
+				
 	<?php else : ?>
 				<h1>Not Found!</h1>
 				<p>Sorry, but you are looking for something that isn't here.</p>
